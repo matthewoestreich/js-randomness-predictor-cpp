95ff2928b640af6659b1a93b9db4270a92d2e817
Matt Oestreich <matthewpoestreich@gmail.com>
2025-06-24 20:20:58 -0500

    update

--- tests/V8.test.cpp
+++ tests/V8.test.cpp
@@ -41,18 +41,32 @@ TEST_CASE("V8Predictor : Node.js Version 24.2.0", "[V8Predictor, v24.2.0]") {
         0.7514661363382521,
         0.9348208735728415
     };
-    try {
-      NodeVersion v24_2_0 = NodeVersion{.major = 24, .minor = 2, .patch = 0};
-      V8Predictor v8_v24_2_0{v24_2_0, sequence_v24_2_0};
-      std::vector<double> predictions_v24_2_0;
+    NodeVersion v24_2_0 = NodeVersion{.major = 24, .minor = 2, .patch = 0};
+    V8Predictor v8_v24_2_0{v24_2_0, sequence_v24_2_0};
+    std::vector<double> predictions_v24_2_0;
 
-      for (size_t i = 0; i < expected_v24_2_0.size(); ++i) {
-        predictions_v24_2_0.push_back(v8_v24_2_0.predictNext());
-      }
-
-      REQUIRE(expected_v24_2_0 == predictions_v24_2_0);
-    } catch (std::exception &e) {
-      std::cerr << "Error [v24.2.0]" << e.what() << "\n";
+    for (size_t i = 0; i < expected_v24_2_0.size(); ++i) {
+      predictions_v24_2_0.push_back(v8_v24_2_0.predictNext());
     }
+
+    REQUIRE(expected_v24_2_0 == predictions_v24_2_0);
+  }
+}
+
+TEST_CASE("V8Predictor : Misc", "[V8Predictor]") {
+  std::vector<double> sequence = {
+      0.01800425609760259, 0.19267361208155598, 0.9892770985784053, 0.49553307275603264, 0.7362624704291061
+  };
+  NodeVersion v = NodeVersion{24, 2, 0};
+  V8Predictor v8 = V8Predictor{v, sequence};
+
+  SECTION("Validate sequence doesn't change") {
+    // Just run a predict next to change internal state.
+    v8.predictNext();
+    REQUIRE(v8.getSequence() == sequence);
+  }
+  SECTION("Validate Node version doesn't change") {
+    NodeVersion nv = v8.getNodeVersion();
+    REQUIRE(nv.toString() == v.toString());
   }
 }
